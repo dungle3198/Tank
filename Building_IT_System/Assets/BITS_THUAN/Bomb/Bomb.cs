8973cc94397122d0a834c1bda3b3367a2719e6d4
unknown <trangnguyenanhthuan@gmail.com>
2019-11-14 12:36:41 +0700

    Bomb, Rotate Spike

---
+++ Building_IT_System/Assets/BITS_THUAN/Bomb/Bomb.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    [SerializeField]
+    protected GameObject explosive_fx;
+    [SerializeField]
+    float damage = 50;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Player>())
+        {
+            if (explosive_fx)
+            {
+                explosive_fx.SetActive(true);
+            }
+            Destroy(this.gameObject);
+            Player player = other.GetComponent<Player>();
+            player.applyDamge(damage, Tank.Team.enemy);
+            if (this.transform.parent.gameObject)
+            {
+                Destroy(this.transform.parent.gameObject, 6f);
+            }
+        }
+    }
+}
