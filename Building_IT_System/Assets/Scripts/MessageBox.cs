8d476976a5b7a7224499e6022eb4e4aa7f5159a7
dungle3198 <dungle3198@users.noreply.github.com>
2019-11-14 10:56:01 +0700

    push hud ui

---
+++ Building_IT_System/Assets/Scripts/MessageBox.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MessageBox : MonoBehaviour
+{
+    [SerializeField]
+    GameObject messagebox;
+
+    private void Start()
+    {
+        if(messagebox)
+        {
+            messagebox.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.GetComponent<Player>())
+        {
+            if (messagebox)
+            {
+                messagebox.SetActive(true);
+                Destroy(gameObject, 5f);
+            }
+            
+        }
+    }
+   
+}
