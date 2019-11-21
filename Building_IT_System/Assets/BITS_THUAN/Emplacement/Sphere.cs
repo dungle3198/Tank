d6df760fbca613e6d4bce77abc48496f12f8510d
unknown <trangnguyenanhthuan@gmail.com>
2019-11-21 13:34:11 +0700

    Level 2

---
+++ Building_IT_System/Assets/BITS_THUAN/Emplacement/Sphere.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sphere : MonoBehaviour
+{
+    [SerializeField]
+    Gun gun1;
+    [SerializeField]
+    Gun gun2;
+    [SerializeField]
+    Gun gun3;
+    [SerializeField]
+    Gun gun4;
+    [SerializeField]
+    float interval = 5;
+    [SerializeField]
+    float shootTime = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        shootTime += Time.deltaTime;
+        if (shootTime > interval)
+        {
+            gun1.Shoot();
+            gun2.Shoot();
+            gun3.Shoot();
+            gun4.Shoot();
+            shootTime = 0;
+        }
+    }
+}
