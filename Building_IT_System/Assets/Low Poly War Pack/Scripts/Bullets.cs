47eb1e66e01e78b490b1622b897218cb62048798
dungle3198 <dungle3198@users.noreply.github.com>
2019-11-25 15:13:13 +0700

    pushing asset

---
+++ Building_IT_System/Assets/Low Poly War Pack/Scripts/Bullets.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullets : MonoBehaviour
+{
+    public float destroyTime;
+
+    public Collider[] toIgnore;
+
+    public GameObject particles;
+
+    [HideInInspector]
+    public bool instantiateParticles = false;
+
+    void Start()
+    {
+        StartCoroutine(AutoDestroy(6));
+        toIgnore = GetComponents<Collider>();
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        foreach (Collider item in toIgnore)
+        {
+            if (instantiateParticles && particles != null)
+            {
+                Instantiate(particles, other.contacts[0].point, new Quaternion(0, 0, 0, 0));
+            }
+
+            else
+            {
+                AutoDestroy(1);
+            }
+        }
+    }
+
+    IEnumerator ExpiryDate(float deathTime )
+    {
+        yield return new WaitForSeconds(deathTime);
+        Destroy(this.gameObject);
+    }
+
+    IEnumerator AutoDestroy(float time)
+    {
+        yield return new WaitForSeconds(time);
+        Destroy(this.gameObject);
+    }
+}
---
+++ Building_IT_System/Assets/Scripts/GameSystem.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSystem : MonoBehaviour
+{
+    int m_Gold = PlayerPrefs.GetInt("gold", 0);
+    int m_Health = PlayerPrefs.GetInt("maxhealth", 0);
+    int m_Damage = PlayerPrefs.GetInt("maxdamage", 0);
+    int m_Ammo = PlayerPrefs.GetInt("maxammo", 0);
+    int m_level = PlayerPrefs.GetInt("maxlevel", 0);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
