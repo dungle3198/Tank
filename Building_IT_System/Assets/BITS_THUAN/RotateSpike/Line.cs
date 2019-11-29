2f622a7828944ac1fb37c0153727ed15cab3bc3d
unknown <trangnguyenanhthuan@gmail.com>
2019-11-29 12:42:14 +0700

    Rotated Spike

---
+++ Building_IT_System/Assets/BITS_THUAN/RotateSpike/Line.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Line : MonoBehaviour
+{
+    [SerializeField]
+    float damage = 50;
+    [SerializeField]
+    float interval = 1;
+    [SerializeField]
+    float stayTime = 0;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Player>())
+        {
+            Player player = other.GetComponent<Player>();
+            player.applyDamge(damage, Tank.Team.enemy);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        stayTime += Time.deltaTime;
+        if (stayTime > interval)
+        {
+            Player player = other.GetComponent<Player>();
+            player.applyDamge(damage, Tank.Team.enemy);
+            stayTime = 0;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        stayTime = 0;
+    }
+}
