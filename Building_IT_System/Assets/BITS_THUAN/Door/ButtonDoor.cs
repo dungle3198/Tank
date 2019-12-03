813a5c987072eda29762dbacd69f3e34814abc78
unknown <trangnguyenanhthuan@gmail.com>
2019-12-03 18:41:11 +0700

    add spike/ door

---
+++ Building_IT_System/Assets/BITS_THUAN/Door/ButtonDoor.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonDoor : MonoBehaviour
+{
+    [SerializeField]
+    Material material;
+    [SerializeField]
+    Renderer rend;
+    [SerializeField]
+    Door door;
+    bool not_opened = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (rend)
+        {
+            rend = GetComponent<Renderer>();
+            rend.enabled = true;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Player>())
+        {
+            if (rend)
+            {
+                if (material) {  
+                    rend.material = material;
+                }
+            }
+            if (door)
+            {
+                if (not_opened)
+                {
+                    door.open();
+                    not_opened = false;
+                }
+            }
+        }
+    }
+}
---
+++ Building_IT_System/Assets/BITS_THUAN/Door/Door.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public void open()
+    {
+        transform.Translate((Vector3.down*6));
+    }
+}
