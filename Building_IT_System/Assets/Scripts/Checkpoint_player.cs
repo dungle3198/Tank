9cf011680aa7b8b65fe0e8885e25b9baaa36dff0
dungle3198 <dungle3198@users.noreply.github.com>
2019-12-25 11:45:16 +0700

    push checkpoint

---
+++ Building_IT_System/Assets/Scripts/Checkpoint_player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint_player : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 checkPos = new Vector3(0, 0, 0);
+    [SerializeField]
+    GameSystem GS;
+    [SerializeField]
+    MeshRenderer mesh;
+    // Start is called before the first frame update
+    void Start()
+    {
+        checkPos = new Vector3(transform.position.x, 0.5f, transform.position.z);
+        if(FindObjectOfType<GameSystem>())
+        {
+            GS = FindObjectOfType<GameSystem>();
+        }
+        if(GetComponent<MeshRenderer>())
+        {
+            mesh = GetComponent<MeshRenderer>();
+            mesh.enabled = false;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.GetComponent<Player>())
+        {
+            if (GS)
+            {
+                GS.setRespawnPos(checkPos);
+            }
+
+        }
+    }
+}
