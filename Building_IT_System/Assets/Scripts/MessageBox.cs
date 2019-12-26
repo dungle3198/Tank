1191e8cf4ee61b56af81dd4b2f51dbe9b2ec6bfd
dungle3198 <dungle3198@users.noreply.github.com>
2019-12-26 13:46:47 +0700

    push volume message

--- Building_IT_System/Assets/Scripts/MessageBox.cs
+++ Building_IT_System/Assets/Scripts/MessageBox.cs
@@ -1,31 +1,56 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class MessageBox : MonoBehaviour
 {
     [SerializeField]
     GameObject messagebox;
-
+    public float distance;
+    public float appearDistance = 10;
+    public string message;
+    [SerializeField] Player player;
+    [SerializeField] SphereCollider col;
+    [SerializeField] Text messageBox; 
+    [SerializeField] float Timer = 10f;
     private void Start()
     {
-        if(messagebox)
+        if (GetComponentInChildren<Text>())
+        {
+            messageBox = GetComponentInChildren<Text>();
+            messageBox.text = message;
+
+        }
+        if (messagebox)
         {
             messagebox.SetActive(false);
         }
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if(other.GetComponent<Player>())
+        if(FindObjectOfType<Player>())
         {
-            if (messagebox)
+            player = FindObjectOfType<Player>();
+        }
+        if(GetComponent<SphereCollider>())
+        {
+            col = GetComponent<SphereCollider>();
+            col.enabled = false;
+        }
+       
+    }
+    private void Update()
+    {
+        if(player)
+        {
+            distance = Vector3.Distance(transform.position, player.transform.position);
+            if(distance <= appearDistance)
             {
-                messagebox.SetActive(true);
-                Destroy(gameObject, 5f);
+                if (messagebox)
+                {
+                    messagebox.SetActive(true);
+                    Destroy(gameObject, Timer );
+                }
             }
-            
         }
     }
+   
    
 }
